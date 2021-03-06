f4da9419b997085d4029b2465492037e036e525f
johnson <phantruongson96@gmail.com>
2021-03-05 23:40:39 -0500

    char Map

--- Object/obj.js
+++ Object/obj.js
@@ -3,3 +3,6 @@ let instructor = {
   isInstructor: true,
   favoriteNumbers: [1,5,7,9]
 }
+console.log(Object.keys(instructor));
+console.log(Object.values(instructor));
+console.log(Object.entries(instructor));
